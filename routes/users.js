8132207036f22a435d1ef29ecb3e0374937bb853
yanggqm <289656703@qq.com>
2018-12-06 23:27:56 +0800

    yang

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 // 引入router
 const router = require('koa-router')();
 
-router.prefix('/users')
+router.prefix('/users');
 
+
+// user父路由对应的处理
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
+
+// user/bar 对应的处理
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
